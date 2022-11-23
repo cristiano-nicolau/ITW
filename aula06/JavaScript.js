8d254bceeb41770e8a91c5b0c678ee25efedc26c
Cristiano Nicolau <115578289+cristiano-nicolau@users.noreply.github.com>
2022-11-23 17:59:29 +0000

    Add files via upload

---
+++ aula06/JavaScript.js
@@ -0,0 +1,43 @@
+﻿var operacao = "+"
+function getOperacao() {
+    var e = document.getElementById("operacao");
+    operacao = e.options[e.selectedIndex].value;
+    console.log(operacao);
+}
+
+function calcula() {
+    var op1 = document.getElementById("op1");
+    var op2 = document.getElementById("op2");
+    var res = document.getElementById("res");
+    switch (operacao) {
+        case "+":
+            res.value = parseFloat(op1.value) + parseFloat(op2.value);
+            break;
+        case "-":
+            res.value = op1.value - op2.value;
+            break;
+        case "*":
+            res.value = op1.value * op2.value;
+            break;
+        case ":":
+            if (op2.value != 0) {
+                res.value = op1.value / op2.value;
+            }
+            else {
+                alert("Erro:Operçao invalida")
+            }
+            break;
+        case "%":
+            res.value = op1.value % op2.value;
+            break;
+        case "!":
+            res.value = 1;
+            for (var i = 2; i <= op1.value; i++) {
+                res.value = res.value * i
+            }
+            break;
+
+        default:
+            alert("Erro:Operçao nao defenida")
+    }
+}
---
+++ aula07/JavaScript.js
@@ -0,0 +1,85 @@
+﻿var start = true;
+var controlo = 1;
+var op1 = "";
+var op2 = "";
+var operacao = "";
+var res = document.getElementById("res");
+
+function addNumber() {
+    var x = event.target.value;
+    if (controlo == 1) {
+        op1 = op1 + x;
+    }
+    else {
+        op2 = op2 + x;
+    }
+    if (start == true) {
+        res.innerText = "";
+        start = false;
+    }
+    res.innerText += x;
+}
+
+function addOperation() {
+    var x = event.target.value;
+    if (operacao == "" && op1 != "") {
+        operacao = x;
+        res.innerText += x;
+        controlo = 2;
+    }
+    else {
+        erro();
+    }
+}
+
+function clearResult() {
+    start = true;
+    controlo = 1;
+    operacao = "";
+    op1 = "";
+    op2 = "";
+    res.innerText = "0";
+}
+
+function calculate() {
+    var error = 0;
+    var op1_value = parseFloat(op1);
+    var op2_value = parseFloat(op2);
+
+    if (controlo == 2 && op2 != "") {
+        switch (operacao) {
+            case "+":
+                res.innerText = parseFloat(op1_value) + parseFloat(op2_value);
+                break;
+            case "-":
+                res.innerText = parseFloat(op1_value) - parseFloat(op2_value);
+                break;
+            case "*":
+                res.innerText = parseFloat(op1_value) * parseFloat(op2_value);
+                break;
+            case "/":
+                if (op2_value == 0) {
+                    erro();
+                    break;
+                }
+                resultado = op1_value / op2_value;
+                break;
+        }
+        op1 = res.innerText;
+        op2 = "";
+        controlo = 1;
+        operacao = "";
+    }
+    else {
+        erro();
+    }
+}
+
+function erro() {
+    start = true;
+    controlo = 1;
+    operacao = "";
+    op1 = "";
+    op2 = "";
+    res.innerText = "Erro";
+}
